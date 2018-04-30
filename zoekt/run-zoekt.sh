baa43554f94d2d206552e7352aad99410cc834b1
Jake Hendy <me@jakehendy.com>
2018-04-30 13:41:57 +0100

    Additional code comments

--- zoekt/run-zoekt.sh
+++ zoekt/run-zoekt.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+# If the user doesn't specify a branch, master is a safe bet
 BRANCHES=${BRANCHES:-master}
+# Explicitly set GOPATH
 export GOPATH=$(go env GOPATH)
+# As a reminder...
 echo $GOPATH
 echo "Indexing branches: ${BRANCHES}"
+# TODO cover off multiple indices/folders
 $GOPATH/bin/zoekt-git-index -branches master /home/git
 
+# TODO Customise Port
 $GOPATH/bin/zoekt-webserver -listen :6070
